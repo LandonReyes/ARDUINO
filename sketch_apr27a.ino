9e1effbf9b04623bf8f993daac1a7016472765fb
LandonReyes <128432899+LandonReyes@users.noreply.github.com>
2023-04-27 11:15:17 -0600

    Add files via upload

---
+++ sketch_apr27a.ino
@@ -0,0 +1,20 @@
+//definir pines de lcd
+#include <LiquidCrystal.h>
+#define RS 2
+#define E 3
+#define D4 4
+#define D5 5
+#define D6 6
+#define D7 7
+
+LiquidCrystal lcd (RS, E, D4, D5, D6, D7);
+
+void setup(){
+lcd.begin(16,2);
+lcd.print("Landon");//poner en la columna 1 fila 1 
+lcd.setCursor(0,1);
+lcd.print("2022012");//poner en la columna 2 fila 1
+}
+void loop (){
+  
+}
